01c05dfa7e2dc5769902a57b1ab39046680a63bd
nmacawile <nmacawile@gmail.com>
2017-02-04 17:56:38 +0800

    refactored player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,34 +17,36 @@ describe TicTacToe::HumanPlayer do
 			is_expected.to receive_message_chain(:gets, :chomp, :to_i) { index }
 		end
 
+		after do
+			subject.turn
+		end
+
 		context "when cell is already marked" do
 			it "doesn't mark cell" do			
-				is_expected.to receive(:puts)
-				allow(index).to receive(:between?).and_return(true)
+				is_expected.to receive(:puts).with(/Invalid/)
+				expect(index).to receive(:between?).and_return(true)
 				cell = double
 				expect(game).to receive_message_chain(:grid, :[]).with(index) { cell }
 				expect(cell).to receive(:mark).with("foo").and_return(false)
-				subject.turn
+			end
+		end
+
+		context "when cell index is invalid" do
+			it "doesn't mark cell" do
+				is_expected.to receive(:puts).with(/Invalid/)
+				expect(index).to receive(:between?).and_return(false)
 			end
 		end
 
 		context "when cell is empty" do
 			it "marks cell" do
+				is_expected.not_to receive(:puts).with(/Invalid/)
 				expect(index).to receive(:between?).and_return(true)				
 				cell = double
 				expect(game).to receive_message_chain(:grid, :[]).with(index) { cell }
 				expect(cell).to receive(:mark).with("foo").and_return(true)
-				subject.turn
 			end
-		end
-
-		context "when cell index is invalid" do
-			it "doesn't mark cell" do	
-				is_expected.to receive(:puts)
-				allow(index).to receive(:between?).and_return(false)
-				subject.turn
-			end
-		end
+		end		
 	end
 end
 
